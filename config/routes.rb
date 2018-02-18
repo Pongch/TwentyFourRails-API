50223576058d798644e9b5d67f230bcc118491bf
YOUR NAME <pong_thailand@hotmail.com>
2018-02-18 15:23:57 +0700

    new note API feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :notes
+  namespace 'api' do
+    resources :twenty_fours
+  end
 end
